ffe4a2c67db4df575ac3146713ff54a0597b4503
xiangxiucheng <xiangxiucheng@ttyun.com>
2016-11-22 15:24:39 +0800

    项目初始化

---
+++ src/app/database/database.go
@@ -0,0 +1,5 @@
+/*  project: database
+    author:diogo
+    time: 2016/11/22-14:41
+*/
+package database
---
+++ src/app/main.go
@@ -0,0 +1,5 @@
+/*  project: app
+    author:diogo
+    time: 2016/11/22-14:25
+*/
+package app
